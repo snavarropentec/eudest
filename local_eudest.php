b99baadcf71fc5c656e0b1e21997e9489626c859
snavarropentec <snavarro@pentec.es>
2017-07-17 09:37:05 +0200

    Prueba 28 convalidate

--- local_eudest.php
+++ local_eudest.php
@@ -1058,11 +1058,11 @@ class local_eudest {
                 foreach ($grades as $grade) {
                     $maxgrade = $grade->finalgrade;
                     // Update grade value.
-                    //if ($record->itemid != null) {
+                    if ($record->itemid != null) {
                         $this->eude_update_course_grade($record->itemid, $record->courseid, $record->userid, $maxgrade,
                                 "convalidation");
-                    //}
-                    break;
+                    }
+                    //break;
                 }
             }
 
